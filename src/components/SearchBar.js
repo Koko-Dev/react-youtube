01a20d76e84ee42422ee8bc017d74da9a1017ee9
Koko-Dev <koko.webdev@gmail.com>
2022-03-22 09:59:43 -0400

    In SearchBar.js, in onFormSubmit function, modify TODO explaining that the callback eventually needed here is to inform the App that the user has just submitted the form and that they probably want to attempt to initiate another search of the youTube API

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,8 +7,11 @@ class SearchBar extends React.Component {
 		this.setState({term: event.target.value});
 		console.log(this.state.term);
 
-		// TODO: Make sure we call callback
-		//  from parent component
+		// TODO: Make sure we call callback from parent
+		//  component so that it tells the App that the
+		//  user just submitted the form and that they
+		//  probably want to attempt to initiate another
+		//  search of the youTube API
 	}
 onFormSubmit = (event) => {
 		event.preventDefault();

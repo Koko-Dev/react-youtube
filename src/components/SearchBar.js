3418091422bba456df00aa16f8e3aa6a44110f5e
Koko-Dev <koko.webdev@gmail.com>
2022-03-22 09:37:07 -0400

    In SearchBar.js, set input value prop to this.state.term and wire up onChange event which points to function onInputChange; in onInputChage() arrow function, the argument will be the event itself and in its body, setState of term to event.target.value

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,13 +3,20 @@ import React from 'react';
 class SearchBar extends React.Component {
 	state = {term: ''}
 
+	onInputChange = (event) => {
+		this.setState({term: event.target.value});
+	}
+
 	render() {
 		return (
 			<div className="search-bar ui segment">
 				<form className="ui form">
 					<div className="field">
 						<label htmlFor="">Video Search</label>
-						<input value={this.state.term} type="text"/>
+						<input
+							value={this.state.term}
+							type="text"
+							onChange={this.onInputChange} />
 					</div>
 				</form>
 			</div>

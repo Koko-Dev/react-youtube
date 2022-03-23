acd41e7b1dd817520f209de7251ca887e513adba
Koko-Dev <koko.webdev@gmail.com>
2022-03-23 13:16:50 -0400

    In App.js, in onTermSubmit, await this.setState videos to response.data.items; to test this, in render() return, add a sentence which logs the length of the array of videos returned from youtube.get('/search') => 'I have {this.state.videos.length} videos'

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
-	state = { videos: [] }
+	state = { videos: [] };
 
 	onTermSubmit = async (term) => {
 		// TODO: Fetch a list of videos according to term
@@ -17,15 +17,17 @@ class App extends React.Component {
 			params: {
 				q: term
 			}
-		})
+		});
 
-		console.log(response.data.items);
+		await this.setState({ videos: response.data.items });
+
 	}
 
 	render() {
 		return (
 			<div className="ui container">
 				<SearchBar onFormSubmit={this.onTermSubmit}/>
+				I have {this.state.videos.length} videos.
 			</div>
 		)
 	}

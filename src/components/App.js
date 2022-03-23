e5916d2e5278dfe66780d46e6d1f79280e0900b6
Koko-Dev <koko.webdev@gmail.com>
2022-03-23 12:59:07 -0400

    In App.js, because youtube is asynchronous, make onTermSubmit an asynchronous function and await youtube.get()

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,15 @@ import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
-	onTermSubmit = (term) => {
-		youtube.get('/search', {
+	onTermSubmit = async (term) => {
+		// TODO: Fetch a list of videos according to term
+		// TODO: Take the videos received and
+		//  set them as state on our App component.
+		// NOTE: This will allow App component to
+		//  rerender/update itself, which is then going
+		//  to allow us to render those newly fetched
+		//  videos out as a list onto the screen.
+		await youtube.get('/search', {
 			params: {
 				q: term
 			}

b3a78ef8a66e984443870da9e647c995ef36a8ec
Koko-Dev <koko.webdev@gmail.com>
2022-03-23 15:33:35 -0400

    In VideoItem.js, add className video-item to main div; also, add div with className 'description' and put the video.snippet.description in its body;  in VideoItem.css, center the text by using display: flex and align-items to center; set cursor to pointer

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const VideoItem = ({ video }) => {
 	return (
-		<div className="item">
+		<div className="item video-item">
 			<img src={video.snippet.thumbnails.medium.url}
 			     className="ui image"/>
 			<div className="content">

5871d2fc0cf5bbb34d167c41d0a423fd64df8bac
Koko-Dev <koko.webdev@gmail.com>
2022-03-24 12:12:34 -0400

    In VideoList.js, in function component VideoList, further destructure the argument to include prop onVideoSelect and add prop to return statement component <VideoItem />

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ({ videos }) => {
-	// NOTE: props.video = array of different records
-	//  of videos that we will render out as HTML onto
-	//  the screen
-	// TODO: Map over the video list and render one
-	//  <VideoItem /> for each video
+const VideoList = ({ videos, onVideoSelect }) => {
 	const renderedList = videos.map((video) => {
-		return <VideoItem video={video}/>;
+		return <VideoItem onVideoSelect={onVideoSelect} video={video}/>;
 	})
 	return (
 		<div className="ui relaxed divided list">{renderedList}</div>

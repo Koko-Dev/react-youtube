f0f31981052fb886574e830d89ab730d48f22831
Koko-Dev <koko.webdev@gmail.com>
2022-03-29 12:03:31 -0400

    In SearchBar.js, modify return div in SearchBar class component to include semantic-ui styling; include video title and video description to the screen;  also, add comments to each step befor return

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+	// step: Initialize state object
 	state = {term: ''}
 
+	// step:  callback event handler for <input> onChange
 	onInputChange = (event) => {
 		this.setState({term: event.target.value});
 	}
 
-onFormSubmit = (event) => {
+	// step: callback event handler for <form> onSubmit
+	onFormSubmit = (event) => {
 		event.preventDefault();
+
+		/* -- note: this.prop.onFormSubmit created in App.js
+			 -- code: From <App /> => Parent Component
+			      <SearchBar onFormSubmit={this.onTermSubmit}
+			 -- note: App.js onTermSubmit receives the term as an argument
+			     and awaits response to youtube /search endpoint with
+			      params { q: term }
+		   -- important: When onSubmit event is triggered, form has been submitted */
 		this.props.onFormSubmit(this.state.term);
 	}
 

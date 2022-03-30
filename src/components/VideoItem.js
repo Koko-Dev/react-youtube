2a9c99f9133edca56dacbf86af1184b3e41f5057
Koko-Dev <koko.webdev@gmail.com>
2022-03-30 09:40:47 -0400

    Refactor VideoItem.js

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -8,9 +8,6 @@ const VideoItem = ({ video, onVideoSelect }) => {
 			     className="ui image"/>
 			<div className="content">
 				<div className="header">{video.snippet.title}</div>
-				{/* /.header */}
-				{/*<div className="description">{video.snippet.description}</div>*/}
-				{/* /.description */}
 			</div>
 			{/* /.content */}
 		</div>

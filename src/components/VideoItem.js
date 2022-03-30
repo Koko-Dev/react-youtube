d46fa89cfd727b59f2928fa7f59b901cbed39c59
Koko-Dev <koko.webdev@gmail.com>
2022-03-30 10:25:14 -0400

    In VideoItem.js, create alt attribute for <img> and set to video title

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -5,6 +5,7 @@ const VideoItem = ({ video, onVideoSelect }) => {
 	return (
 		<div onClick={() => onVideoSelect(video)} className="item video-item">
 			<img src={video.snippet.thumbnails.medium.url}
+			     alt={video.snippet.title}
 			     className="ui image"/>
 			<div className="content">
 				<div className="header">{video.snippet.title}</div>
